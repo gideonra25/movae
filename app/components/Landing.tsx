262161394a40eb09b042b2b633462e1225e5c8b6
Gideon Ramot <gideonnoedigp2504@students.unnes.ac.id>
2025-08-05 22:19:56 +0700

    Update Landing.tsx

--- app/components/Landing.tsx
+++ app/components/Landing.tsx
@@ -8,16 +8,16 @@ import movimg7 from "../../public/mov7.jpg"
 
 const Landing = () => {
     return (
-        <div className="items-center justify-center content-center">
-            <img src={movimg1.src} alt="" />
-            <img src={movimg2.src} alt="" />
-            <img src={movimg3.src} alt="" />
-            <img src={movimg4.src} alt="" />
-            <img src={movimg5.src} alt="" />
-            <img src={movimg6.src} alt="" />
-            <img src={movimg7.src} alt="" />
-            <div className="title text-8xl font-semibold mix-blend-exclusion text-white  z-40 fixed top-1/2 left-1/2 -translate-1/2">MOVAE</div>
-            <div className="title text-3xl font-semibold mix-blend-exclusion text-white  z-40 fixed top-5 left-1/2 -translate-1/2">MOVAE</div>
+        <div className="flex items-center justify-center content-center min-h-screen min-w-screen relative bg-black">
+            <img src={movimg1.src} alt="" className="w-64 absolute translate-x-100 translate-y-56" />
+            <img src={movimg2.src} alt="" className="w-64 absolute -translate-x-90 translate-y-50" />
+            <img src={movimg3.src} alt="" className="w-64 absolute translate-x-60 -translate-y-50" />
+            <img src={movimg4.src} alt="" className="w-64 absolute -translate-x-75 -translate-y-65" />
+            <img src={movimg5.src} alt="" className="w-64 absolute translate-x-115" />
+            <img src={movimg6.src} alt="" className="w-64 absolute -translate-x-130" />
+            <img src={movimg7.src} alt="" className="w-64 absolute translate-y-65" />
+            <div className="title text-8xl font-semibold mix-blend-exclusion text-white z-20 fixed top-1/2 left-1/2 -translate-1/2">MOVAE</div>
+            {/* <div className="title text-3xl font-semibold mix-blend-exclusion text-white z-40 fixed top-5 left-1/2 -translate-1/2">MOVAE</div> */}
         </div>
     );
 };
